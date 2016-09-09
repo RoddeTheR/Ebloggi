957042d93b689056e4f299700f14275c373abe8f
nataxe <natalie.axelsson@live.se>
2016-09-09 13:10:12 +0200

    Testelitest

--- script.js
+++ script.js
@@ -23,6 +23,20 @@ $(document).ready(function(){
 	$("#postwindow").click(function(){
 		event.stopPropagation();
 	})
+
+
+	makeFeedMessage(1,"2016-08-29");
+	makeFeedMessage(462,"2016-08-30");
+	makeFeedMessage(8,"2016-08-31");
+	makeFeedMessage(70,"2016-09-01");
+	makeFeedMessage(279,"2016-09-02");
+	makeFeedMessage(52,"2016-09-03");
+	makeFeedMessage(711,"2016-09-04");
+	makeFeedMessage(69,"2016-09-05");
+	makeFeedMessage(66,"2016-09-06");
+	makeFeedMessage(184,"2016-09-07");
+	makeFeedMessage(182,"2016-09-08");
+
 });
 
 function createEmojiTable(){
@@ -82,27 +96,47 @@ function isActive(tag){
 function getAnswer(){
 	var input = document.getElementById("userInput").value.toLowerCase();
 	console.log(":D");
-	if("syjsjysjssikdikdikdikduikduikduikduikduikdikduikduikdui"===input){
-		var i = 0;
-		while(true){
-			console.log(":D");
-			i++
-			if ( i > 100000){
-				break
+
+	switch (input) {
+		case "syjsjysjssikdikdikdikduikduikduikduikduikdikduikduikdui":
+			var i = 0;
+			while(true){
+				console.log(":D");
+				i++
+				if ( i > 100000){
+					break
+				}
 			}
-		}
-	}
-	if(input === "random"){
-		var a = random(0,1037)
-		while ( $.inArray(a,activeTags) >= 0){
-			a = random(0,1037)
-		}
-		alert(a.toString())
+		
+		case "random":
+			var a = random(0,1037)
+			while ( $.inArray(a,activeTags) >= 0){
+				a = random(0,1037)
+			}
+			alert(a.toString())
 	}
 
 };
 
-
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function makeFeedMessage(tag, dateStr){
+	var messageStr = "<div class='post'><p>" + dateStr + "</p><div class='sentMessage'>";
+
+	var randomNum = random(1,10);
+	for (i=0; i<randomNum; i++){
+		messageStr += "<img src='Images/iOS/" + tag.toString() + ".png'>"
+	}
+	messageStr += "</div></div>";
+
+	$("#feed").append(messageStr);
+}
+
+
+function makePost(tag, dateStr, iosRank, msgRank, ssRank, text, weight){
+	activeTags.push(tag)
+	makeFeedMessage(tag, dateStr);
 }
