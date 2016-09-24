27dce41abb74538ed40be30494dc2ef048e15f88
rrg <elmejorrrg@gmail.com>
2016-09-24 22:32:21 +0200

    score thingymabob

---
+++ Posts/fun.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+
+dirs = os.listdir("./")
+total = 0
+iOSsc = 0
+samsc = 0
+messc = 0
+
+
+for fil in dirs:
+	if fil.endswith(".txt"):
+		if fil != "output.txt" :
+			#print(fil)
+			t = open(fil,'r').read().split('\n')
+			total = total + 1
+			#judgea inte mina 6 if satser
+			if   t[3] == "M" :
+				messc = messc +  2 * int(t[2])
+			elif t[3] == "A" :
+				iOSsc = iOSsc +  2 * int(t[2])
+			else :
+				samsc = samsc +  2 * int(t[2])
+
+			if   t[4] == "M" :
+				messc = messc + int(t[2])
+			elif t[4] == "A" :
+				iOSsc = iOSsc + int(t[2])
+			else :
+				samsc = samsc + int(t[2])
+
+			#print(t)
+
+result ="Emojis:"+str(total)+ "\n" + "iOS:"+str(iOSsc)+" Samsung:"+str(samsc)+" Messenger:" +str(messc) + "\n"
+print(result)
+
+ff = open('output.txt','w')
+ff.write(result)
